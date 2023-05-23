d2e586718f0d0a45bff48e6bde67063379e1b25d
PranishSthaa <prince99912@gmail.com>
2023-05-23 08:17:37 +0545

    -UI changes in login and register pages

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +49,133 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('layouts.guest')
+
+@section('content')
+    <main class="w-full max-w-md mx-auto p-6">
+        <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
+            <div class="p-4 sm:p-7">
+                <div class="text-center">
+                    <h1 class="block text-2xl font-bold text-gray-800">Sign up</h1>
+                    <p class="mt-2 text-sm text-gray-600">
+                        Already have an account?
+                        <a class="text-red-600 decoration-2 hover:underline font-medium" href="{{ route('login') }}">
+                            Login here
+                        </a>
+                    </p>
+                </div>
+
+                <div class="mt-5">
+                    <a href="{{ url('/') }}">
+                        <button type="button"
+                            class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-red-600 transition-all text-sm">
+                            <span class="material-symbols-outlined">
+                                arrow_back
+                            </span>
+                            Go Back
+                        </button>
+                    </a>
+                    <!-- Form -->
+                    <form method="POST" action="{{ route('register') }}" class="mt-4">
+                        @csrf
+                        <div class="grid gap-y-4">
+                            <!-- Form Group -->
+                            <div>
+                                <label for="name" class="block text-sm mb-2">Name</label>
+                                <div class="relative">
+                                    <input type="text" id="name" name="name" value="{{ old('name') }}"
+                                        class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-red-500 focus:ring-red-500"
+                                        required aria-describedby="email-error" required autofocus autocomplete="name">
+                                    {{-- <div
+                                        class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                                        <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor"
+                                            viewBox="0 0 16 16" aria-hidden="true">
+                                            <path
+                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                        </svg>
+                                    </div> --}}
+                                </div>
+                                {{-- <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email
+                                    address so we can get back to you</p> --}}
+                            </div>
+                            <!-- End Form Group -->
+
+                            <!-- Form Group -->
+                            <div>
+                                <label for="email" class="block text-sm mb-2">Email</label>
+                                <div class="relative">
+                                    <input type="email" id="email" name="email" value="{{ old('email') }}"
+                                        class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-red-500 focus:ring-red-500"
+                                        required aria-describedby="email-error" required autofocus autocomplete="username">
+                                    {{-- <div
+                                        class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                                        <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor"
+                                            viewBox="0 0 16 16" aria-hidden="true">
+                                            <path
+                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                        </svg>
+                                    </div> --}}
+                                </div>
+                                {{-- <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email
+                                    address so we can get back to you</p> --}}
+                            </div>
+                            <!-- End Form Group -->
+
+                            <!-- Form Group -->
+                            <div>
+                                <div class="flex justify-between items-center">
+                                    <label for="password" class="block text-sm mb-2">Password</label>
+                                    {{-- <a class="text-sm text-red-600 decoration-2 hover:underline font-medium"
+                                        href="../examples/html/recover-account.html">Forgot password?</a> --}}
+                                </div>
+                                <div class="relative">
+                                    <input type="password" id="password" name="password"
+                                        class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-red-500 focus:ring-red-500"
+                                        required aria-describedby="password-error" autocomplete="new-password">
+                                    {{-- <div
+                                        class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                                        <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor"
+                                            viewBox="0 0 16 16" aria-hidden="true">
+                                            <path
+                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                        </svg>
+                                    </div> --}}
+                                </div>
+                                {{-- <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p> --}}
+                            </div>
+                            <!-- End Form Group -->
+
+                            <!-- Form Group -->
+                            <div>
+                                <div class="flex justify-between items-center">
+                                    <label for="password_confirmation" class="block text-sm mb-2">Confirm Password</label>
+                                </div>
+                                <div class="relative">
+                                    <input type="password" id="password_confirmation" name="password_confirmation"
+                                        class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-red-500 focus:ring-red-500"
+                                        required aria-describedby="password-error" autocomplete="new-password">
+                                    {{-- <div
+                                        class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                                        <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor"
+                                            viewBox="0 0 16 16" aria-hidden="true">
+                                            <path
+                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                                        </svg>
+                                    </div> --}}
+                                </div>
+                                {{-- <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p> --}}
+                            </div>
+                            <!-- End Form Group -->
+
+                            <button type="submit"
+                                class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-all text-sm">Register</button>
+                        </div>
+                    </form>
+                    <!-- End Form -->
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
--- resources/views/livewire/frontend/navbar.blade.php
+++ resources/views/livewire/frontend/navbar.blade.php
@@ -3,7 +3,7 @@
     <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b text-sm py-2.5 sm:py-4">
         <nav class="max-w-7xl flex basis-full items-center w-full mx-auto px-4 sm:px-6 lg:px-8" aria-label="Global">
             <div class="mr-5 md:mr-8">
-                <a class="flex-none text-xl font-semibold" href="#" aria-label="Brand">Restaurant Recom</a>
+                <a class="flex-none text-xl font-semibold" href="{{url('/')}}" aria-label="Brand">Restaurant Recom</a>
             </div>
 
             <div class="w-full flex items-center justify-end ml-auto sm:justify-between sm:gap-x-3 sm:order-3">
@@ -11,7 +11,7 @@
                     <label for="icon" class="sr-only">Search</label>
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none z-20 pl-4">
-                            <span class="material-symbols-outlined">
+                            <span class="material-symbols-outlined text-red-500">
                                 search
                             </span>
                         </div>
@@ -25,19 +25,27 @@
                     @auth
                         <div class="hs-dropdown relative inline-flex" data-hs-dropdown-placement="bottom-right">
                             <button id="hs-dropdown-with-header" type="button"
-                                class="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 text-xs">
-                                <img class="inline-block h-[2.375rem] w-[2.375rem] rounded-full ring-2 ring-white"
-                                    src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-                                    alt="Image Description">
+                                class="hs-dropdown-toggle py-2 px-2 inline-flex justify-center items-center gap-1 rounded-md border-2 border-red-200 font-semibold text-red-500 hover:text-white hover:bg-red-500 hover:border-red-500 focus:outline-none focus:ring-2 focus:ring-red-200 focus:ring-offset-2 transition-all text-sm">
+                                <span class="material-symbols-outlined">
+                                    person
+                                </span>
+                                My Account
                             </button>
 
                             <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] z-10 bg-white shadow-md rounded-lg p-2"
                                 aria-labelledby="hs-dropdown-with-header">
                                 <div class="py-3 px-5 -m-2 bg-gray-100 rounded-t-lg">
                                     <p class="text-sm text-gray-500">Signed in as</p>
-                                    <p class="text-sm font-medium text-gray-800">james@site.com</p>
+                                    <p class="text-sm font-medium text-gray-800">{{ auth()->user()->email }}</p>
                                 </div>
                                 <div class="mt-2 py-2 first:pt-0 last:pb-0">
+                                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-red-500 hover:text-white focus:ring-2 focus:ring-red-500"
+                                        href="#">
+                                        <span class="material-symbols-outlined">
+                                            face
+                                        </span>
+                                        View Profile
+                                    </a>
                                     <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-red-500 hover:text-white focus:ring-2 focus:ring-red-500"
                                         href="#">
                                         <span class="material-symbols-outlined">
@@ -45,13 +53,17 @@
                                         </span>
                                         My Reviews
                                     </a>
-                                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-red-500 hover:text-white focus:ring-2 focus:ring-red-500"
-                                        href="#">
-                                        <span class="material-symbols-outlined">
-                                            logout
-                                        </span>
-                                        Logout
-                                    </a>
+                                    <form action="{{ route('logout') }}" method="post">
+                                        @csrf
+                                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-red-500 hover:text-white focus:ring-2 focus:ring-red-500"
+                                            href="{{ route('logout') }}"
+                                            onclick="event.preventDefault(); this.closest('form').submit();">
+                                            <span class="material-symbols-outlined">
+                                                logout
+                                            </span>
+                                            Logout
+                                        </a>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -68,15 +80,13 @@
                         <a href="{{ route('register') }}">
                             <button type="button"
                                 class="py-2 px-2 inline-flex justify-center items-center gap-1 rounded-md bg-red-100 border border-transparent font-semibold text-red-500 hover:text-white hover:bg-red-500 focus:outline-none focus:ring-2 ring-offset-white focus:ring-red-500 focus:ring-offset-2 transition-all text-sm">
-                                <span class="material-symbols-outlined">
-                                    person_add
-                                </span>
                                 Signup
+                                <span class="material-symbols-outlined">
+                                    arrow_right_alt
+                                </span>
                             </button>
                         </a>
                     @endauth
-
-
                 </div>
             </div>
         </nav>
@@ -91,9 +101,6 @@
             </div>
             <div class="snap-center shrink-0 pr-5 sm:pr-8 sm:last:pr-0">
                 <a class="inline-flex items-center gap-x-2 hover:text-red-500" href="#">Cuisines</a>
-            </div>
-            <div class="snap-center shrink-0 pr-5 sm:pr-8 sm:last:pr-0">
-                <a class="inline-flex items-center gap-x-2 hover:text-red-500" href="#">My Reviews</a>
             </div>
             <div class="snap-center shrink-0 pr-5 sm:pr-8 sm:last:pr-0">
                 <a class="inline-flex items-center gap-x-2 hover:text-red-500" href="#">About</a>
